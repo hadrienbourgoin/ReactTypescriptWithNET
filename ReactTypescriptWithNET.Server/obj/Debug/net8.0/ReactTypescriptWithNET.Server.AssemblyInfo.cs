b23121a4fbd7a9af9b64b7c4258977242b916738
Hadrien Bourgoin <h.bourgoin@lyon.teamsquare.fr>
2024-06-20 09:40:19 +0200

    sql server local

--- ReactTypescriptWithNET.Server/obj/Debug/net8.0/ReactTypescriptWithNET.Server.AssemblyInfo.cs
+++ ReactTypescriptWithNET.Server/obj/Debug/net8.0/ReactTypescriptWithNET.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactTypescriptWithNET.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c710c2a524a82eee88949a03f93a0ba18424ece4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactTypescriptWithNET.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactTypescriptWithNET.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
